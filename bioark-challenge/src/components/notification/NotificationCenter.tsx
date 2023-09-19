6e33de5d54990262993e5aa44636dd88aafda31c
nimarhmti <nimarahmati2000@gmail.com>
2023-09-19 12:26:25 +0330

    added

--- bioark-challenge/src/components/notification/NotificationCenter.tsx
+++ bioark-challenge/src/components/notification/NotificationCenter.tsx
@@ -4,10 +4,8 @@ export const NotificationCenter = () => {
   return (
     <div className="content relative">
       <Notification />
-      <div className="absolute h-96 left-1 top-full bg-slate-500 w-96 z-20 overflow-y-auto">
-        <div className="relative bg-red-300 w-full top-0 left-0 z-10">
-          header
-        </div>
+      <div className="flex flex-col absolute justify-between h-96 left-1 top-full bg-slate-500 w-96 z-20 overflow-x-auto">
+        <div className=" bg-red-300 w-full top-0 left-0 z-10">header</div>
         <ul className="overflow-scroll">
           <li>yt</li>
           <li>yt</li>
@@ -26,9 +24,7 @@ export const NotificationCenter = () => {
           <li>yt</li>
           <li>yt</li>
         </ul>
-        <div className="absolute  bottom-0 w-full bg-red-400 left-0 z-20">
-          footer
-        </div>
+        <div className="  bottom-0 w-full bg-red-400 left-0 z-20">footer</div>
       </div>
     </div>
   );
