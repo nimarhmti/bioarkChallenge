2a89c745b104a9d7c3e42f5aeee26e9489518793
nimarhmti <nimarahmati2000@gmail.com>
2023-09-20 01:52:31 +0330

    header added

--- bioark-challenge/src/App.tsx
+++ bioark-challenge/src/App.tsx
@@ -1,9 +1,11 @@
+import { Header } from "./components/ui/header/Header";
 import { Navbar } from "./components/ui/nav/navbar";
 
 function App() {
   return (
-    <main className="h-screen ">
+    <main className="h-screen relative">
       <Navbar />
+      <Header />
     </main>
   );
 }
---
+++ bioark-challenge/src/components/ui/header/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className="absolute top-14 bg-slate-200 w-full h-14 z-[1400]"></div>
+  );
+};
