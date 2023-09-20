18ce6051ea724a65be9c1efbf81c6f543038e396
nimarhmti <nimarahmati2000@gmail.com>
2023-09-20 12:27:54 +0330

    tab added

--- bioark-challenge/src/components/ui/header/Header.tsx
+++ bioark-challenge/src/components/ui/header/Header.tsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import "./style.css";
 export const Header = () => {
   return (
-    <div className="absolute top-14 bg-slate-200 w-full h-14 z-[1400]"></div>
+    <div className="absolute top-14 shadow-md w-full h-14 z-[1400] flex items-end lg:px-20 xl:px-48 p-2">
+      <ul className="flex gap-6 text-sm">
+        <li></li>
+        <li>test</li>
+        <li>test</li>
+        <li>test</li>
+      </ul>
+    </div>
   );
 };
---
+++ bioark-challenge/src/config/tab.ts
@@ -0,0 +1,748 @@
+export const tab = [
+  {
+    id: "1",
+    title: "پنل من",
+    submenu: [
+      {
+        id: "a1",
+        title: "داشبورد",
+      },
+      {
+        id: "a2",
+        title: "بصری سازی",
+      },
+      {
+        id: "a3",
+        title: "گزارش",
+        submenu: [
+          {
+            id: "aa1",
+            title: "گزارش فرم",
+          },
+          {
+            id: "aa2",
+            title: "گزارش سفارشی",
+          },
+          {
+            id: "aa3",
+            title: "گزارش نسخه",
+          },
+        ],
+      },
+      {
+        id: "a4",
+        title: "بیمار ها",
+      },
+      {
+        id: "a5",
+        title: "ویزیت ها",
+      },
+      {
+        id: "a5",
+        title: "ارجاعات",
+      },
+      {
+        id: "a5",
+        title: " سطل آشغال",
+      },
+      {
+        id: "a6",
+        title: " مجوز های خاص ",
+      },
+    ],
+  },
+  {
+    id: "2",
+    title: "بیماران من",
+  },
+  {
+    id: "3",
+    title: "مدیریت",
+    submenu: [
+      {
+        id: "b1",
+        title: "داشبورد",
+        submenu: [
+          { id: "bb1", title: "مدیریت" },
+          { id: "bb2", title: "پرونده الکترونیک" },
+        ],
+      },
+      {
+        id: "b2",
+        title: "بیمار ها",
+      },
+      {
+        id: "b3",
+        title: "ویزیت ها",
+        submenu: [
+          { id: "bb3", title: "ویزیت کامل" },
+          { id: "bb4", title: "ویزیت خلاصه" },
+          { id: "bb5", title: "ویزیت ناقص" },
+          { id: "bb6", title: "ویزیت معلق" },
+        ],
+      },
+      {
+        id: "b4",
+        title: "سطل اشغال پیشرفته",
+      },
+      {
+        id: "b5",
+        title: "پیامک ها",
+        submenu: [
+          { id: "bb7", title: "لاگ پیامک ها" },
+          { id: "bb8", title: "ارسال پیامک به کاربران" },
+          { id: "bb9", title: "ارسال پیامک به بیماران" },
+          { id: "bb10", title: "پرونده الکترونیک" },
+        ],
+      },
+      {
+        id: "b6",
+        title: "لاگ",
+      },
+      {
+        id: "b7",
+        title: "مدیریت جلسات",
+      },
+      {
+        id: "b8",
+        title: "اتاق عمل",
+        submenu: [
+          {
+            id: "bb11",
+            title: "مدیر اتاق عمل",
+          },
+          {
+            id: "bb12",
+            title: "لیست انتظار اتاق عمل",
+          },
+          {
+            id: "bb13",
+            title: "لیست انتظار من",
+          },
+          {
+            id: "bb14",
+            title: "درخواست تخت",
+          },
+          {
+            id: "bb15",
+            title: "لیست بررسی",
+          },
+          {
+            id: "bb16",
+            title: "روتین ",
+          },
+          {
+            id: "bb17",
+            title: "زمان بندی جراحان ",
+          },
+          {
+            id: "bb18",
+            title: "اتاق عمل",
+          },
+          {
+            id: "bb19",
+            title: "نوع عمل",
+          },
+        ],
+      },
+      {
+        id: "b9",
+        title: "تخت",
+        submenu: [
+          {
+            id: "bb20",
+            title: "مدیر تخت",
+          },
+          {
+            id: "bb21",
+            title: "درخواست تخت",
+          },
+          {
+            id: "bb22",
+            title: "اتاق",
+          },
+          {
+            id: "bb23",
+            title: "طبقه ",
+          },
+          {
+            id: "bb24",
+            title: "ساختمان",
+          },
+          {
+            id: "bb25",
+            title: "نوع تخت",
+          },
+          {
+            id: "bb26",
+            title: "گزارش",
+          },
+          {
+            id: "bb27",
+            title: "تخضیض تخت",
+          },
+        ],
+      },
+      {
+        id: "b10",
+        title: "اعلان ها",
+      },
+      {
+        id: "b11",
+        title: " مانیتورینگ",
+      },
+      {
+        id: "b12",
+        title: " گردش کار",
+      },
+      {
+        id: "b13",
+        title: "پذیرش",
+        submenu: [
+          { id: "bb28", title: "بستری" },
+          { id: "bb29", title: "پذیرش" },
+          { id: "bb30", title: "تریاش" },
+        ],
+      },
+    ],
+  },
+  {
+    id: "3",
+    title: "اعضا",
+    submenu: [
+      { id: "c1", title: "گاربران" },
+      { id: "c2", title: "کابران کلینیک" },
+      { id: "c3", title: "کابران مهمان" },
+      { id: "c4", title: "درخواست عضویت" },
+      { id: "c5", title: "مراکز" },
+      { id: "c6", title: "بخش ها" },
+      { id: "c7", title: "گروه درمانگاهی" },
+      {
+        id: "c8",
+        title: "تخصص",
+        submenu: [
+          {
+            id: "cc1",
+            title: "مدیریت تخصص",
+          },
+          {
+            id: "cc2",
+            title: "بیماری-تخصص",
+          },
+          {
+            id: "cc3",
+            title: "شکایت-تخصص",
+          },
+        ],
+      },
+      { id: "c9", title: "گروه های کاربران" },
+    ],
+  },
+  {
+    id: "4",
+    title: "بیماری ها",
+    submenu: [
+      {
+        id: "d1",
+        title: "مدیرییت بیماری ",
+      },
+      {
+        id: "d2",
+        title: "دارو",
+        submenu: [
+          { id: "dd1", title: "مدیریت دارو ها" },
+          { id: "dd2", title: "مدیریت کارخانه دارو" },
+          { id: "dd3", title: "مدیریت دسته دارو" },
+          { id: "dd4", title: "برند دارو" },
+        ],
+      },
+      {
+        id: "d3",
+        title: "GV",
+        submenu: [
+          {
+            id: "dd5",
+            title: "شکایت",
+          },
+          {
+            id: "dd6",
+            title: "تشخیص",
+          },
+          {
+            id: "dd7",
+            title: "ROS",
+          },
+          {
+            id: "dd8",
+            title: "PMH",
+          },
+          {
+            id: "dd9",
+            title: "توصیه",
+          },
+          {
+            id: "dd10",
+            title: "ازمایش",
+            submenu: [
+              { id: "add10", titile: "آزمایش" },
+              { id: "bdd10", titile: "فیلد های ازمایش " },
+            ],
+          },
+          {
+            id: "dd11",
+            title: "اسناد",
+            submenu: [
+              { id: "add11", titile: "اسناد" },
+              { id: "bdd11", titile: " فیلد های اسناد" },
+            ],
+          },
+          {
+            id: "dd12",
+            title: "پرونده الکترونیک",
+            submenu: [
+              { id: "add12", titile: "فرم/گرافی" },
+              { id: "bdd12", titile: " فیلد های فرم" },
+              { id: "cdd12", titile: "نقاط گرافی" },
+            ],
+          },
+          {
+            id: "dd13",
+            title: "معاینات بالینی",
+            submenu: [
+              { id: "add13", titile: "فرم/گرافی" },
+              { id: "bdd13", titile: " فیلد های فرم" },
+              { id: "cdd13", titile: "نقاط گرافی" },
+              { id: "cdd13", titile: " نشانه ها" },
+            ],
+          },
+          {
+            id: "dd14",
+            title: "عمل ",
+            submenu: [
+              { id: "add14", titile: "فرم/گرافی" },
+              { id: "bdd14", titile: " فیلد های فرم" },
+              { id: "cdd14", titile: "نقاط گرافی" },
+            ],
+          },
+          {
+            id: "dd15",
+            title: "خدمات",
+            submenu: [
+              {
+                id: "add15",
+                title: "Deliver Services",
+                submenu: [
+                  { id: "aadd15", titile: "فرم/گرافی" },
+                  { id: "bbdd15", titile: " فیلد های فرم" },
+                  { id: "ccdd15", titile: "نقاط گرافی" },
+                ],
+              },
+              {
+                id: "bdd15",
+                title: "orderPair-clinic",
+                submenu: [
+                  { id: "eadd15", titile: "فرم/گرافی" },
+                  { id: "fbdd15", titile: " فیلد های فرم" },
+                  { id: "gcdd15", titile: "نقاط گرافی" },
+                ],
+              },
+              {
+                id: "cdd15",
+                title: "Order Interventoin",
+                submenu: [
+                  { id: "hadd15", titile: "فرم/گرافی" },
+                  { id: "ibdd15", titile: " فیلد های فرم" },
+                  { id: "jcdd15", titile: "نقاط گرافی" },
+                ],
+              },
+            ],
+          },
+          {
+            id: "dd16",
+            title: "شاخه های GV",
+          },
+          {
+            id: "dd17",
+            title: "GV Tags",
+          },
+        ],
+      },
+      {
+        id: "d3",
+        title: "مدیریت شغل",
+      },
+      {
+        id: "d4",
+        title: "آزمایش",
+      },
+      {
+        id: "d4",
+        title: "آزمایش CD",
+      },
+      {
+        id: "d5",
+        title: "عکس",
+        submenu: [
+          { id: "ad5", title: "نوع تصویر" },
+          { id: "bd5", title: "نقاط تصویر" },
+          { id: "cd5", title: "گزینه های تصویر" },
+          { id: "dd5", title: "فرم ها" },
+          { id: "ed5", title: "فیلد های فرم" },
+        ],
+      },
+      {
+        id: "d6",
+        title: "اسناد اشتراکی",
+      },
+    ],
+  },
+  {
+    id: "5",
+    title: "صفحه اول",
+    submenue: [
+      {
+        id: "d1",
+        titile: "سالاید ها",
+      },
+      {
+        id: "d2",
+        titile: "بیماری ها",
+      },
+      {
+        id: "d3",
+        titile: "خبر ها",
+      },
+      {
+        id: "d4",
+        titile: "پرسش و پاسخ های متداول",
+      },
+      {
+        id: "d5",
+        titile: "اعضای تیم",
+      },
+      {
+        id: "d6",
+        titile: "مقالات",
+      },
+      {
+        id: "d7",
+        titile: "داستان کوتاه ",
+      },
+      {
+        id: "d8",
+        titile: "سایر تظیمات",
+      },
+      {
+        id: "d9",
+        titile: "باز خورد های مشتریان",
+      },
+      {
+        id: "d10",
+        titile: "گالری",
+      },
+      {
+        id: "d11",
+        titile: "مشتریان",
+      },
+      {
+        id: "d12",
+        titile: "گواهی نامه",
+      },
+    ],
+  },
+  {
+    id: "6",
+    titlr: "مالی",
+    submenu: [
+      {
+        id: "e1",
+        title: "بیمه",
+        submenu: [
+          { id: "ae1", title: "ضریب خدمت" },
+          { id: "be1", title: " خدمت" },
+          { id: "ce1", title: "Global Services" },
+          { id: "de1", title: "بیمه" },
+          { id: "fe1", title: "گروه بندی خدمات بیمه" },
+          { id: "ge1", title: "پکیج بیمه" },
+          { id: "he1", title: "گروه بندی خدمات سراسری" },
+        ],
+      },
+      {
+        id: "e2",
+        title: "مالی",
+        submenu: [
+          { id: "ae2", title: "مدیریت صندوق" },
+          { id: "be2", title: "صندوق" },
+        ],
+      },
+      {
+        id: "e3",
+        title: "اشتراک",
+        submenu: [
+          { id: "ae3", title: "اشتراک ها" },
+          { id: "be3", title: "پرداخت ها" },
+          { id: "ce3", title: "کلاس های اشتراک" },
+          { id: "de3", title: "پیشنهادات" },
+        ],
+      },
+      {
+        id: "e4",
+        title: "لینک پرداخت",
+        submenu: [
+          { id: "ae4", title: "تصویه لینک پرداخت" },
+          { id: "be4", title: " تنظیمات کاربر" },
+          { id: "ce4", title: "وضعیت لینک پرداخت" },
+        ],
+      },
+      {
+        id: "e5",
+        title: "گزارش",
+      },
+      {
+        id: "e6",
+        title: "تنظیمات کارتخوان",
+      },
+      {
+        id: "e7",
+        title: "تنظیمات پرداخت",
+        submenu: [
+          {
+            id: "ae7",
+            title: "تارخچه پرداخت",
+          },
+          {
+            id: "be7",
+            title: "تنظیمات  درگاه",
+          },
+        ],
+      },
+      {
+        id: "e8",
+        title: "مالی تخت",
+      },
+    ],
+  },
+  {
+    id: "7",
+    title: "مطب",
+    submenu: [
+      { id: "f1", title: "پزشک" },
+      { id: "f2", title: "چکاپ" },
+      { id: "f3", title: "تقویم" },
+      {
+        id: "f4",
+        title: "مانیتورنیگ",
+        submenu: [
+          {
+            id: "af4",
+            title: "نسخه",
+          },
+          {
+            id: "bf4",
+            title: "پروند الکترونیک",
+          },
+        ],
+      },
+      {
+        id: "f5",
+        title: "تنظیمات مطب",
+        submenu: [
+          {
+            id: "af5",
+            title: "خدمت",
+          },
+          {
+            id: "bf5",
+            title: "بهای بیمه",
+          },
+          {
+            id: "bf5",
+            title: " آزمایش",
+          },
+          {
+            id: "bf5",
+            title: "یادآوری ",
+          },
+        ],
+      },
+      { id: "f6", title: "دفترچه تلفن" },
+      { id: "f7", title: "دفتر نوبت" },
+      { id: "f8", title: "پیامک" },
+      {
+        id: "f9",
+        title: "پذیرش",
+        submenu: [
+          {
+            id: "af9",
+            title: "پنل",
+          },
+          {
+            id: "bf9",
+            title: "ماشین تیکت",
+          },
+          {
+            id: "زf9",
+            title: "پذیرش",
+          },
+        ],
+      },
+      {
+        id: "f10",
+        title: "مالی",
+        submenu: [
+          {
+            id: "af10",
+            title: "کیف پول",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "8",
+    title: "بایوویزیت",
+    submenu: [
+      {
+        id: "h1",
+        title: "مدیدیت",
+        submenu: [
+          {
+            id: "ah1",
+            title: "نوبت های پزشک",
+          },
+          {
+            id: "bh1",
+            title: "نوبت های درمانگاه",
+          },
+          {
+            id: "ch1",
+            title: "سوالات  متداول",
+          },
+          {
+            id: "dh1",
+            title: "سیاست های نوبتدهی",
+          },
+          {
+            id: "eh1",
+            title: "ثبت  نظر",
+          },
+          {
+            id: "fh1",
+            title: "خدمت",
+          },
+          {
+            id: "gh1",
+            title: "ویدیو",
+          },
+          {
+            id: "hh1",
+            title: "عکس",
+          },
+          {
+            id: "ih1",
+            title: "مدیر پزشکان ",
+          },
+          {
+            id: "jh1",
+            title: "مدیر درمانگاها",
+          },
+          {
+            id: "kh1",
+            title: "بیماران بایوویزیت",
+          },
+          {
+            id: "lh1",
+            title: "تراکنش های بانکی ",
+            submenu: [
+              { id: "alh1", title: "نوبت دهی" },
+              { id: "blh1", title: "کیف پول" },
+            ],
+          },
+          {
+            id: "mh1",
+            title: "شکایات",
+            submenu: [
+              { id: "amh1", title: "کاربران وبسایت" },
+              { id: "bmh1", title: "دیگر کابران" },
+            ],
+          },
+          {
+            id: "nh1",
+            title: "مالی",
+            submenu: [
+              { id: "anh1", title: "بیمار ها " },
+              { id: "bnh1", title: "پزشک" },
+              { id: "cnh1", title: "پرداخت علی الحساب" },
+            ],
+          },
+          {
+            id: "oh1",
+            title: "تنظیمات",
+            submenu: [
+              { id: "aoh1", title: "قوانین استفاده " },
+              { id: "boh1", title: "تسویه حساب" },
+            ],
+          },
+        ],
+      },
+      {
+        id: "h2",
+        title: "مدیریت نوبت ها",
+      },
+      {
+        id: "h3",
+        title: "تنظیمات زمان",
+      },
+      {
+        id: "h4",
+        title: "تنظیمات ویزیت",
+        submeu: [
+          {
+            id: "ah4",
+            title: "مدیریت بایو مگ",
+          },
+          {
+            id: "bh4",
+            title: "مدیریت دسته بندی",
+          },
+          {
+            id: "ch4",
+            title: "مدیریت نویسندگان بایومگ",
+          },
+        ],
+      },
+      {
+        id: "h5",
+        title: "مدیران بایو مگ",
+      },
+      {
+        id: "h6",
+        title: "تنظیمات پروفایل ",
+      },
+      {
+        id: "h7",
+        title: "نظرات",
+      },
+      {
+        id: "h8",
+        title: "اطلاعات بانکی",
+      },
+      {
+        id: "h9",
+        title: "وبسایت بایو ویزیت",
+      },
+      {
+        id: "h10",
+        title: "اموزش بایو ویزیت",
+      },
+      {
+        id: "h11",
+        title: "درامد",
+      },
+      {
+        id: "h12",
+        title: "مدیریت ویدیو",
+      },
+    ],
+  },
+];
